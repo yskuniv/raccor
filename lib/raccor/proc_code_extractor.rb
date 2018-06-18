94c0a70f270fc6295ba29438c7675f9a4f0ec8f5
ysk_univ <ysk.univ.1007@gmail.com>
2018-06-18 18:43:43 +0900

    impl ProcCodeExtractor

---
+++ lib/raccor/proc_code_extractor.rb
@@ -0,0 +1,323 @@
+module ProcCodeExtractor
+  require 'ripper'
+
+  class ParseHelper
+    module Enumerable
+      class EnumerableBuffer
+        def initialize(enum)
+          @enum_wrapper = Enumerator.new { |y|
+            enum.each do |item| y << item; end
+          }
+          @buffer = []
+        end
+
+        def fetch(nth, ifnone = nil)
+          begin
+            prepare_to_fetch(nth)
+          rescue IndexError => e
+            return (ifnone || raise(e))
+          end
+
+          @buffer[nth]
+        end
+
+        def at(nth)
+          begin
+            fetch(nth)
+          rescue IndexError
+            nil
+          end
+        end
+
+        def sub(nth)
+          prepare_to_fetch(nth)
+
+          Enumerator.new { |y|
+            nth.step do |i|
+              y << begin
+                     fetch(i)
+                   rescue IndexError
+                     break
+                   end
+            end
+          }.lazy
+        end
+
+        alias :[] :at
+
+
+        private
+
+        def prepare_to_fetch(nth)
+          n = [nth - @buffer.count + 1, 0].max
+          n.times do
+            @buffer << begin
+                         @enum_wrapper.next
+                       rescue StopIteration
+                         raise IndexError.new
+                       end
+          end
+        end
+      end
+
+
+      def fold_map(init = nil, &block)
+        init_, self_ = init ? [init, self] : [first, drop(1)]
+
+        s = init_
+        self_.map { |item|
+          s = block[s, item]
+          s
+        }
+      end
+
+      def cons_iterator(&block)
+        eb = EnumerableBuffer.new(self)
+
+        Enumerator.new { |y|
+          0.step do |nth|
+            y << begin
+                   eb.sub(nth)
+                 rescue IndexError
+                   break
+                 end
+          end
+        }.lazy
+      end
+    end
+
+
+    class Token
+      def initialize(token)
+        pos, event, ident = token
+
+        @pos = pos
+        @event = event
+        @ident = ident
+      end
+
+      attr_reader :pos, :event, :ident
+    end
+
+    class TokenMatcher
+      class Element
+        def initialize(event: nil, ident: nil)
+          @event = event
+          @ident = ident
+        end
+
+        def match?(token)
+          token &&
+            (@event ? @event == token.event : true) &&
+            (@ident ? @ident == token.ident : true)
+        end
+      end
+
+      class TreeRoot
+        def initialize(children = [])
+          @children = children
+        end
+
+        def match?(tokens)
+          @children.empty? ? true : @children.any? { |node| node.match?(tokens) }
+        end
+
+        attr_accessor :children
+      end
+
+      class TreeNode
+        def initialize(element, children = [])
+          @element = element
+          @children = children
+        end
+
+        def match?(tokens)
+          head, tail = tokens.first, tokens.drop(1)
+
+          @element.match?(head) &&
+            (@children.empty? ? true : @children.any? { |node| node.match?(tail) })
+        end
+
+        attr_accessor :element, :children
+      end
+
+      class << self
+        alias :[] :new
+      end
+
+
+      def initialize(*sequence)
+        children = parse_sequence(sequence)
+        @tree_root = TreeRoot.new(children)
+      end
+
+      def match?(tokens, strict_match: false)
+        tokens_ = case strict_match
+                  when false
+                    return false if ignored_token?(tokens.first)
+                    tokens.reject { |token| ignored_token?(token) }
+                  else
+                    tokens
+                  end
+
+        @tree_root.match?(tokens_)
+      end
+
+      alias :=~ :match?
+
+
+      private
+
+      def parse_sequence(sequence)
+        dummy_root = TreeRoot.new
+
+        current_parents = [dummy_root]
+        loop do
+          x = sequence.shift or break
+
+          parent = (current_parents.count == 1) ? current_parents.first : raise(NotImplementedError.new)
+
+          parent.children = case x
+                            when Array
+                              parse_patterns(x)
+                            else
+                              [parse_item(x)]
+                            end
+
+          current_parents = parent.children
+        end
+
+        dummy_root.children
+      end
+
+      def parse_patterns(patterns)
+        patterns.flat_map { |ptn|
+          case ptn
+          when Array
+            parse_sequence(ptn)
+          else
+            [parse_item(ptn)]
+          end
+        }
+      end
+
+      def parse_item(x)
+        arg = case x
+              when String
+                { ident: x }
+              when Hash
+                x
+              else
+                raise TypeError.new
+              end
+
+        TreeNode.new(Element.new(arg))
+      end
+
+      def ignored_token?(token)
+        [
+          Element.new(:event => :on_sp),
+          Element.new(:event => :on_ignored_nl),
+          Element.new(:event => :on_comment),
+        ].each_with_object(token).any?(&:match?)
+      end
+    end
+
+    class SexpMatcher
+      class << self
+        alias :[] :new
+      end
+
+
+      def initialize(*pattern_list)
+        @pattern_list = pattern_list
+      end
+
+      def match?(sexp)
+        return false unless sexp
+
+        @pattern_list.any? { |pattern| submatch(pattern, sexp) }
+      end
+
+      alias :=~ :match?
+
+
+      private
+
+      def submatch(pattern, sexp)
+        return false unless pattern.is_a?(Array) && sexp.is_a?(Array) &&
+                            pattern.count <= sexp.count
+
+        pattern.zip(sexp).all? { |itm_p, itm_s|
+          case itm_p
+          when Array
+            submatch(itm_p, itm_s)
+          else
+            itm_p ? itm_p == itm_s : true
+          end
+        }
+      end
+    end
+
+
+    class << self
+      def token_iterator(path)
+        src = open(path)
+
+        Ripper.lex(src).
+          map { |raw| Token.new(raw) }.
+          extend(ParseHelper::Enumerable).cons_iterator
+      end
+    end
+  end
+
+  class ExtractionError < StandardError
+    def initialize
+      super('multiple procs found on the target line')
+    end
+  end
+
+
+  class << self
+    BeginningOfProcPattern = ParseHelper::TokenMatcher[
+      [
+        ['Proc', '.', 'new', ['do',
+                              '{']],
+        ['proc', ['do',
+                  '{']],
+        ['lambda', ['do',
+                    '{']],
+        ['->', ['do',
+                '{']]
+      ]
+    ]
+
+    ProcCodePattern = ParseHelper::SexpMatcher[
+      [:program, [[:method_add_block]]],
+      [:program, [[:lambda]]],
+    ]
+
+
+    def extract(proc)
+      proc_filepath, proc_linum = proc.source_location
+
+      iter = ParseHelper.token_iterator(proc_filepath).
+               # take only tokens on the target line
+               select { |tokens| token_linum, _ = tokens.first.pos; token_linum == proc_linum }.
+               # trim unnecessary tokens before the proc
+               drop_while { |tokens| BeginningOfProcPattern !~ tokens }
+
+      # find and return the entire proc code
+      res = iter.first.
+              map(&:ident).
+              extend(ParseHelper::Enumerable).fold_map('', &:+).
+              find { |str| ProcCodePattern =~ Ripper.sexp(str) }
+
+      # validate if no other procs exist on the target line
+      raise ExtractionError.new if iter.drop(1).
+                                     find { |tokens| BeginningOfProcPattern =~ tokens }
+
+      res
+    end
+  end
+end
